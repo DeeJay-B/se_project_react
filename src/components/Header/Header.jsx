ce6ba052ffa579c235f37867de07a7eb8cbbdcdb
DeeJay-B <dejonbell@gmail.com>
2025-01-01 17:53:24 -0500

    header and weather cards rendering

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+import logo from "../../assets/header_logo.svg";
+import avatar from "../../assets/header_avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">Date, & Location</p>
+
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance Tegegne</p>
+        <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,12 @@
+import "./WeatherCard.css";
+import sunny from "../../assets/group 120.png";
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75 &deg; F</p>
+      <img src={sunny} alt="" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
