522ef46d10fccc35bda1c9249e7fffbc6b946208
DeeJay-B <dejonbell@gmail.com>
2025-01-05 15:24:42 -0500

    modal functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,19 @@ import "./Header.css";
 import logo from "../../assets/header_logo.svg";
 import avatar from "../../assets/header_avatar.png";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} />
       <p className="header__date-and-location">Date, & Location</p>
 
-      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add Clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terrance Tegegne</p>
         <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,28 +1,27 @@
-function ModalWithForm() {
+import "./ModalWithForm.css";
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
-    <div className="modal">
-      <form className="modal__form">
-        <h2 className="modal__title">New Garment</h2>
-        <buttton className="modal__close">close</buttton>
-        <label htmlFor="name" className="modal__label">
-          Name{" "}
-          <input
-            type="text"
-            className="modal__input"
-            id="name"
-            placeholder="Name"
-          />{" "}
-        </label>
-        <label htmlFor="imageUrl" className="modal__label">
-          Image{" "}
-          <input
-            type="url"
-            className="modal__input"
-            id="imageUrl"
-            placeholder="Image URL"
-          />
-        </label>
-      </form>
+    <div className={'modal ${activeModal === "add-garment" && "modal_opened"}'}>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button className="modal__close">close</button>
+        <form className="modal__form">
+          {children}
+          <button
+            onClick={handleCloseClick}
+            type="submit"
+            className="modal__submit"
+          >
+            {buttonText}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
