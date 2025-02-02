0523bcf09cc8c8eb30a56263d4be4d9abc41b79e
DeeJay-B <dejonbell@gmail.com>
2025-02-01 21:57:28 -0500

    working on delete confirmation style

--- src/components/ConfirmModal/ConfirmModal.jsx
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -1,24 +1,27 @@
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import "./ConfirmModal.css";
 
 const ConfirmModal = ({ isOpen, onClose, onSubmit }) => {
   return (
-    <ModalWithForm
-      title="Are you sure you want to delete this item?
+    <div>
+      <ModalWithForm
+        title="Are you sure you want to delete this item?
 This action is irreversible."
-      buttonText="Yes, delete item"
-      isOpen={isOpen}
-      onClose={onClose}
-      onSubmit={onSubmit}
-    >
-      <div className="modal__confirm-btns">
-        <button type="submit" className="modal__submit">
-          Yes, delete item
-        </button>
-        <button onClick={onClose} type="button" className="modal__submit">
-          Cancel
-        </button>
-      </div>
-    </ModalWithForm>
+        buttonText="Yes, delete item"
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={onSubmit}
+      >
+        <div className="modal__confirm-btns">
+          <button type="submit" className="modal__submit">
+            Yes, delete item
+          </button>
+          <button onClick={onClose} type="button" className="modal__submit">
+            Cancel
+          </button>
+        </div>
+      </ModalWithForm>
+    </div>
   );
 };
 
